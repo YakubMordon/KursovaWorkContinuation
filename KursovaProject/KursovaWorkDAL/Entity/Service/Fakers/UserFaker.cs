18ea21fbe1be2aae1ccdcceed5361ed8df31cbab
Jacob Mordon <YakubKazimiezh.Mordon.KN.2021@edu.lpnu.ua>
2024-06-28 19:45:48 +0300

    Fixed Faker.

--- KursovaProject/KursovaWorkDAL/Entity/Service/Fakers/UserFaker.cs
+++ KursovaProject/KursovaWorkDAL/Entity/Service/Fakers/UserFaker.cs
@@ -14,7 +14,6 @@ public sealed class UserFaker : Faker<User>
     public UserFaker()
     {
         UseSeed(1994);
-        RuleFor(entity => entity.Id, faker => faker.IndexFaker);
         RuleFor(entity => entity.FirstName, faker => faker.Person.FirstName);
         RuleFor(entity => entity.LastName, faker => faker.Person.LastName);
         RuleFor(entity => entity.Email, faker => faker.Person.Email);
