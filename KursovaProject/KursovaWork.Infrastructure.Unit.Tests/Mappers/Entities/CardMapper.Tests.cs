1c24b2c5aeda250dac65689aa68c552ca11d0cd0
Yakub Mordon <yakubkazimiezh.mordon.kn.2021@lpnu.ua>
2024-06-30 12:00:04 +0300

    Wrote tests for CardMapper

---
+++ KursovaProject/KursovaWork.Infrastructure.Unit.Tests/Mappers/Entities/CardMapper.Tests.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using KursovaWork.Domain.Entities;
+using KursovaWork.Domain.Models;
+using Mapper = KursovaWork.Infrastructure.Mappers.Entities.CardMapper;
+
+namespace KursovaWork.Infrastructure.Unit.Tests.Mappers.Entities;
+
+public class CardMapper
+{
+    private readonly Mapper _mapper;
+
+    public CardMapper()
+    {
+        _mapper = new Mapper();
+    }
+
+    [Fact]
+    public void EntityToModel_ShouldMapCardToCreditCardViewModel()
+    {
+        // Arrange
+
+        var entity = new Card
+        {
+            UserId = 1,
+            CardNumber = "1111 2222 3333 4444",
+            CardHolderName = "Name",
+            ExpirationMonth = "11",
+            ExpirationYear = "24",
+            Cvv = "123"
+        };
+
+        // Act
+
+        var actual = _mapper.EntityToModel(entity);
+
+        // Assert
+
+        actual.UserId.Should().Be(entity.UserId);
+
+        actual.CardNumber.Should().Be(entity.CardNumber);
+
+        actual.CardHolderName.Should().Be(entity.CardHolderName);
+
+        actual.ExpirationMonth.Should().Be(entity.ExpirationMonth);
+
+        actual.ExpirationYear.Should().Be(entity.ExpirationYear);
+
+        actual.Cvv.Should().Be(entity.Cvv);
+    }
+
+    [Fact]
+    public void ModelToEntity_ShouldMapCreditCardViewModelToCard()
+    {
+        // Arrange
+
+        var model = new CreditCardViewModel
+        {
+            UserId = 1,
+            CardNumber = "1111 2222 3333 4444",
+            CardHolderName = "Name",
+            ExpirationMonth = "11",
+            ExpirationYear = "24",
+            Cvv = "123"
+        };
+
+        // Act
+
+        var actual = _mapper.ModelToEntity(model);
+
+        // Assert
+
+        actual.UserId.Should().Be(model.UserId);
+
+        actual.CardNumber.Should().Be(model.CardNumber);
+
+        actual.CardHolderName.Should().Be(model.CardHolderName);
+
+        actual.ExpirationMonth.Should().Be(model.ExpirationMonth);
+
+        actual.ExpirationYear.Should().Be(model.ExpirationYear);
+
+        actual.Cvv.Should().Be(model.Cvv);
+
+        actual.User.Should().BeNull();
+    }
+}
